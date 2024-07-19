f434f1ec201c3291214fbdc17b657fa992d85139
Dhruv Sharma <149249311+dhruv0050@users.noreply.github.com>
2024-07-19 12:41:12 +0530

    Add files via upload

---
+++ Jarvis py/musiclibrary.py
@@ -0,0 +1,7 @@
+music={
+    "soulmate":"https://youtu.be/k3ijQJjUbTs?feature=shared&t=14",
+    "mexico":"https://youtu.be/2sAzb3kraoQ?feature=shared",
+    "softly":"https://youtu.be/cWMxCE2HTag?feature=shared",
+    "GodDamn":"https://youtu.be/k3ijQJjUbTs?feature=shared",
+    "Bars":"https://youtu.be/4DfVxVeqk2o?feature=shared",
+}
